dd1cca94d5bcba445e0f1eb1128ab458780b02a3
ecalder6 <ecalder6@jhu.edu>
2017-05-13 17:07:09 -0400

    last commit.

--- src/run.cmd
+++ src/run.cmd
@@ -3,54 +3,54 @@ ECHO "twitter_seq2seq"
 REM python train.py --cov_path=analytics/twitter_seq2seq --use_mutual=False --use_vae=False --use_highway=False --kl_anneal=False --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_seq2seq.txt
 
 ECHO "twitter_vae"
-REM python train.py --cov_path=analytics/twitter_vae --use_mutual=False --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae.txt
+python train.py --cov_path=analytics/twitter_vae --use_mutual=False --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=True --task=twitter --iterations=5000 --output_file=../data/outputs/twitter_vae.txt
 
 ECHO "twitter_vae_anneal"
 REM python train.py --cov_path=analytics/twitter_vae_anneal --use_mutual=False --use_vae=True --use_highway=False --kl_anneal=True --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae.txt
 
 ECHO "twitter_vae_highway"
-REM python train.py --cov_path=analytics/twitter_vae_highway --use_mutual=False --use_vae=True --use_highway=True --kl_anneal=False --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_highway.txt
+python train.py --cov_path=analytics/twitter_vae_highway --use_mutual=False --use_vae=True --use_highway=True --kl_anneal=False --use_checkpoint=True --task=twitter --iterations=5000 --output_file=../data/outputs/twitter_vae_highway.txt
 
 ECHO "twitter_vae_highway_anneal"
-REM python train.py --cov_path=analytics/twitter_vae_highway_anneal --use_mutual=False --use_vae=True --use_highway=True --kl_anneal=True --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_highway_anneal.txt
+python train.py --cov_path=analytics/twitter_vae_highway_anneal --use_mutual=False --use_vae=True --use_highway=True --kl_anneal=True --use_checkpoint=True --task=twitter --iterations=5000 --output_file=../data/outputs/twitter_vae_highway_anneal.txt
 
-ECHO "twitter_vae_mutual"
-python train.py --cov_path=analytics/twitter_vae_mutual1 --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual1.txt
-python train.py --cov_path=analytics/twitter_vae_mutual2 --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual2.txt
-python train.py --cov_path=analytics/twitter_vae_mutual3 --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual3.txt
+REM ECHO "twitter_vae_mutual"
+REM python train.py --cov_path=analytics/twitter_vae_mutual1 --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual1.txt
+REM python train.py --cov_path=analytics/twitter_vae_mutual2 --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual2.txt
+REM python train.py --cov_path=analytics/twitter_vae_mutual3 --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual3.txt
 
-ECHO "twitter_vae_mutual_anneal"
-python train.py --cov_path=analytics/twitter_vae_mutual_anneal --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=True --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual_anneal.txt
+REM ECHO "twitter_vae_mutual_anneal"
+REM python train.py --cov_path=analytics/twitter_vae_mutual_anneal --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=True --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual_anneal.txt
 
-ECHO "twitter_vae_mutual_highway"
-python train.py --cov_path=analytics/twitter_vae_mutual_highway --use_mutual=True --use_vae=True --use_highway=True --kl_anneal=False --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual_highway.txt
+REM ECHO "twitter_vae_mutual_highway"
+REM python train.py --cov_path=analytics/twitter_vae_mutual_highway --use_mutual=True --use_vae=True --use_highway=True --kl_anneal=False --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual_highway.txt
 
-ECHO "twitter_vae_mutual_highway_anneal"
-python train.py --cov_path=analytics/twitter_vae_mutual_highway_anneal --use_mutual=True --use_vae=True --use_highway=True --kl_anneal=True --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual_highway_anneal.txt
+REM ECHO "twitter_vae_mutual_highway_anneal"
+REM python train.py --cov_path=analytics/twitter_vae_mutual_highway_anneal --use_mutual=True --use_vae=True --use_highway=True --kl_anneal=True --use_checkpoint=False --task=twitter --iterations=2000 --output_file=../data/outputs/twitter_vae_mutual_highway_anneal.txt
 
-ECHO "movie_seq2seq"
-REM python train.py --cov_path=analytics/movie_seq2seq --use_mutual=False --use_vae=False --use_highway=False --kl_anneal=False --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_seq2seq.txt
+REM ECHO "movie_seq2seq"
+REM REM python train.py --cov_path=analytics/movie_seq2seq --use_mutual=False --use_vae=False --use_highway=False --kl_anneal=False --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_seq2seq.txt
 
-ECHO "movie_vae"
-python train.py --cov_path=analytics/movie_vae --use_mutual=False --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae.txt
+REM ECHO "movie_vae"
+REM python train.py --cov_path=analytics/movie_vae --use_mutual=False --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae.txt
 
-ECHO "twitter_movie_vae_annealseq2seq"
-python train.py --cov_path=analytics/movie_vae_anneal --use_mutual=False --use_vae=True --use_highway=False --kl_anneal=True --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae.txt
+REM ECHO "twitter_movie_vae_annealseq2seq"
+REM python train.py --cov_path=analytics/movie_vae_anneal --use_mutual=False --use_vae=True --use_highway=False --kl_anneal=True --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae.txt
 
-ECHO "movie_vae_highway"
-python train.py --cov_path=analytics/movie_vae_highway --use_mutual=False --use_vae=True --use_highway=True --kl_anneal=False --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_highway.txt
+REM ECHO "movie_vae_highway"
+REM python train.py --cov_path=analytics/movie_vae_highway --use_mutual=False --use_vae=True --use_highway=True --kl_anneal=False --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_highway.txt
 
-ECHO "movie_vae_highway_anneal"
-python train.py --cov_path=analytics/movie_vae_highway_anneal --use_mutual=False --use_vae=True --use_highway=True --kl_anneal=True --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_highway_anneal.txt
+REM ECHO "movie_vae_highway_anneal"
+REM python train.py --cov_path=analytics/movie_vae_highway_anneal --use_mutual=False --use_vae=True --use_highway=True --kl_anneal=True --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_highway_anneal.txt
 
-ECHO "movie_vae_mutual"
-python train.py --cov_path=analytics/movie_vae_mutual --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_mutual.txt
+REM ECHO "movie_vae_mutual"
+REM python train.py --cov_path=analytics/movie_vae_mutual --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=False --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_mutual.txt
 
-ECHO "movie_vae_mutual_anneal"
-python train.py --cov_path=analytics/movie_vae_mutual_anneal --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=True --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_mutual_anneal.txt
+REM ECHO "movie_vae_mutual_anneal"
+REM python train.py --cov_path=analytics/movie_vae_mutual_anneal --use_mutual=True --use_vae=True --use_highway=False --kl_anneal=True --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_mutual_anneal.txt
 
-ECHO "movie_vae_mutual_highway"
-python train.py --cov_path=analytics/movie_vae_mutual_highway --use_mutual=True --use_vae=True --use_highway=True --kl_anneal=False --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_mutual_highway.txt
+REM ECHO "movie_vae_mutual_highway"
+REM python train.py --cov_path=analytics/movie_vae_mutual_highway --use_mutual=True --use_vae=True --use_highway=True --kl_anneal=False --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_mutual_highway.txt
 
-ECHO "movie_vae_mutual_highway_anneal"
-python train.py --cov_path=analytics/movie_vae_mutual_highway_anneal --use_mutual=True --use_vae=True --use_highway=True --kl_anneal=True --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_mutual_highway_anneal.txt
+REM ECHO "movie_vae_mutual_highway_anneal"
+REM python train.py --cov_path=analytics/movie_vae_mutual_highway_anneal --use_mutual=True --use_vae=True --use_highway=True --kl_anneal=True --use_checkpoint=False --task=movie --iterations=2000 --output_file=../data/outputs/movie_vae_mutual_highway_anneal.txt
